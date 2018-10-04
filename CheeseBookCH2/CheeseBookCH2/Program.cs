773fae5583654654da57d0d38e67f4fb14660c7d
et8111 <owiesny1018@gmail.com>
2018-10-04 09:35:49 -0400

    testing et8111

--- CheeseBookCH2/CheeseBookCH2/Program.cs
+++ CheeseBookCH2/CheeseBookCH2/Program.cs
@@ -10,10 +10,33 @@ namespace CheeseBookCH2
     {
         static void Main(string[] args)
         {
-            char t = '1';
-            int i = (int) t + 2;
-            char s = (char)i;
-            Console.Write(s);
+            string str = "Indivisibility";
+            if (string.IsNullOrEmpty(str))
+                Console.WriteLine("DSF");
+            Dictionary<char, int> d = new Dictionary<char, int>();
+            str = str.ToUpper();
+            for (int i = 0; i < str.Length; i++)
+            {
+                try { d.Add(str[i], 1); }
+                catch { d[str[i]]++; }
+            }
+            List<int> list = new List<int>(d.Values);
+            int temp = 0;
+            foreach (int i in list)
+                temp = (i >= 2) ? temp += 1 : temp;
+            Console.WriteLine(temp);
+
+            var t = str.GroupBy(c => c).Where(a => a.Count() > 2);
+            foreach (var v in t)
+            {
+                Console.WriteLine(v.Key);
+            }
+            Console.WriteLine();
+
+            string num = "10011010010";
+            Console.WriteLine(num.Count(c => c == '1'));
+
+
         }
     }
 }
